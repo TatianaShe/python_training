14a15fc1c9ce88cfdb5e6fc4251c00fd297dce86
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2021-11-12 21:30:32 +0300

    Выделен класс для работы с контактами (ContactHelper), внесены изменения в соответствующие тесты.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,95 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
+import pytest
 from model.contact import Contact
+from fixture.application import Application
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
     
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_add_new_page(wd)
-        self.create_contact(wd, Contact(firstname="Sergey", middlename="Petrovich", lastname="Ivanov", nickname="drummer", company="Mail", address="Moscow, Lenina st., 3 - 9",
-                            mobile="89111234567", email="ivanov@pochta.ru", bday="4", bmonth="May", byear="1980"))
-        self.logout(wd)
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Sergey", middlename="Petrovich", lastname="Ivanov", nickname="drummer", company="Mail", address="Moscow, Lenina st., 3 - 9",
+                               mobile="89111234567", email="ivanov@pochta.ru", bday="4", bmonth="May", byear="1980"))
+    app.session.logout()
 
-    def test_add_contact_with_empty_middlename(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_add_new_page(wd)
-        self.create_contact(wd, Contact(firstname="Anton", middlename="", lastname="Suvorov", nickname="tonton", company="Lenta", address="Tula, Pravdy st., 8 - 12",
-                            mobile="89214567896", email="tonton@pochta.ru", bday="21", bmonth="July", byear="1994"))
-        self.logout(wd)
-
-    def logout(self, wd):
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def create_contact(self, wd, contact):
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        #wd.find_element_by_css_selector("option[value=\"1\"]").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        #wd.find_element_by_css_selector("option[value=\"May\"]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        # submit contact creation
-        wd.find_element_by_name("submit").click()
-
-    def open_add_new_page(self, wd):
-        # open add new page
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        # login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-
-    def open_home_page(self, wd):
-        # open home page
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact_with_empty_middlename(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Anton", middlename="", lastname="Suvorov", nickname="tonton", company="Lenta", address="Tula, Pravdy st., 8 - 12",
+                               mobile="89214567896", email="tonton@pochta.ru", bday="21", bmonth="July", byear="1994"))
+    app.session.logout()
