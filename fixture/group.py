8ef469e9408bb8be75acc151ab4dd41a742e5d02
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2021-11-14 01:29:00 +0300

    Добавлены тесты модификации группы.

--- fixture/group.py
+++ fixture/group.py
@@ -32,7 +32,28 @@ class GroupHelper:
         # select first group
         wd.find_element_by_name("selected[]").click()
         # submit deletion
-        wd.find_element_by_name("delete")
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # init group edit
+        wd.find_element_by_name("edit").click()
+        # edit group name
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit edit
+        wd.find_element_by_name("update").click()
         self.return_to_groups_page()
 
     def return_to_groups_page(self):
