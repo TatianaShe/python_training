5f6de561aab5042fb8a117d7ce67ad58e273158a
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2021-11-14 22:26:44 +0300

    Добавлены тесты модификации контакта (изменение данных контакта).

--- fixture/contact.py
+++ fixture/contact.py
@@ -56,13 +56,54 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         # submit deletion
         wd.find_element_by_css_selector("[value = 'Delete']")
+        #close pop-up
+        wd.switch_to.alert.accept()
 
-    def add_first_contact_to_group(self, addtogroup):
+    def add_first_contact_to_group(self, add_to_group):
         wd = self.app.wd
         # select first contact
         wd.find_element_by_name("selected[]").click()
         # select group
         wd.find_element_by_name("to_group").click()
-        Select(wd.find_element_by_name("to_group")).select_by_visible_text(addtogroup.to_group)
+        Select(wd.find_element_by_name("to_group")).select_by_visible_text(add_to_group.to_group)
         # submit add
         wd.find_element_by_name("add").click()
+
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        # init contact edit
+        wd.find_element_by_css_selector("a[href*='edit.php?id=1']").click()
+        # edit contact
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        # submit edit
+        wd.find_element_by_name("update").click()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.contact import AddToGroup
+from model.contact import Contact
 
 
 def test_add_first_contact_to_group(app):
     app.session.login(username="admin", password="secret")
     app.contact.add_first_contact_to_group(AddToGroup(to_group="Group for Add"))
+    app.session.logout()
+
+def test_edit_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(
+        Contact(firstname="Alexander", middlename="Sergeevich", lastname="Pushkin", nickname="poet", company="",
+                address="St. Petersburg, Nevsky av.",
+                mobile="", email="", bday="6", bmonth="June", byear="1799"))
     app.session.logout()
