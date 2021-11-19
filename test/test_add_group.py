d961658d560e97d97b1e1a6ea62aed347dedfd49
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2021-11-19 20:01:56 +0300

    Создана интеллектуальная фикстура с проверкой валидности.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,12 +3,8 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="First Group", header="Leader of First Group", footer="Footer of First Group"))
-    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
