98bd2ede5ac9471f5b29261d21e33468fb9afad6
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2021-11-27 01:05:00 +0300

    Загружена информация из тестируемого приложения в виде списков.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,10 +5,16 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group for deletion", header="something", footer="anything else"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name="Newest Group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group for deletion", header="something", footer="anything else"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header="Newest Header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
