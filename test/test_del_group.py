2717ecc61b560dacc97473ade73c3abf03d0bdcf
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2021-12-18 23:09:34 +0300

    Группы: отключены проверки данных пользовательского интерфейса.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,9 +3,9 @@ import random
 from model.group import Group
 
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     if len(db.get_group_list()) == 0:
-        app.group.create(Group(name="Group for deletion"))
+        app.group.create(Group(name="Group for deletion", header="Header", footer="Footer"))
     old_groups = db.get_group_list()
     group = random.choice(old_groups)
     app.group.delete_group_by_id(group.id)
@@ -13,3 +13,5 @@ def test_delete_some_group(app, db):
     new_groups = db.get_group_list()
     old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
