303e2185f32e7b19813cb29480cc0d250b61b009
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2021-12-18 23:58:15 +0300

    Контакты: извлечен список контактов из базы данных, добавлено условие.

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 from model.group import Group
+from model.contact import Contact
 
 
 class DbFixture:
@@ -12,16 +13,28 @@ class DbFixture:
         self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
 
     def get_group_list(self):
-        list = []
+        group_list = []
         cursor = self.connection.cursor()
         try:
             cursor.execute("SELECT group_id, group_name, group_header, group_footer FROM group_list")
             for row in cursor:
                 (id, name, header, footer) = row
-                list.append(Group(name=name, header=header, footer=footer, id=str(id)))
+                group_list.append(Group(name=name, header=header, footer=footer, id=str(id)))
         finally:
             cursor.close()
-        return list
+        return group_list
+
+    def get_contact_list(self):
+        contact_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("SELECT id, firstname, lastname FROM addressbook WHERE deprecated ='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                contact_list.append(Contact(firstname=firstname, lastname=lastname, id=str(id)))
+        finally:
+            cursor.close()
+        return contact_list
 
     def destroy(self):
         self.connection.close()
