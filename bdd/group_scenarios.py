02670062388e742a86b0f38fac070c8c5b46094b
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2022-01-07 21:43:50 +0300

    BDD: обновлены файлы шагов и запуска.

--- bdd/group_scenarios.py
+++ bdd/group_scenarios.py
@@ -1,12 +1,12 @@
 from pytest_bdd import scenario
-from bdd.group_steps import *
+from .group_steps import *
 
 
-@scenario('groups.feature', 'Add new group')
+@scenario("groups.feature", "Add new group")
 def test_add_new_group():
     pass
 
 
-@scenario('groups.feature', 'Delete a group')
+@scenario("groups.feature", "Delete a group")
 def test_delete_group():
     pass
--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -7,13 +7,16 @@ import random
 def group_list(db):
     return db.get_group_list()
 
+
 @given('a group with <name>, <header> and <footer>')
 def new_group(name, header, footer):
     return Group(name=name, header=header, footer=footer)
 
+
 @when('I add the group to the list')
 def add_new_group(app, new_group):
     app.group.create(new_group)
+
 
 @then('the new group list is equal to the old list with the added group')
 def verify_group_added(db, group_list, new_group):
@@ -29,13 +32,16 @@ def non_empty_group_list(db, app):
         app.group.create(Group(name="some group"))
     return db.get_group_list()
 
+
 @given('a random group from the list')
 def random_group(non_empty_group_list):
     return random.choice(non_empty_group_list)
 
+
 @when('I delete the group from the list')
 def delete_group(app, random_group):
     app.group.delete_group_by_id(random_group.id)
+
 
 @then('the new group list is equal to the old list without the deleted group')
 def verify_group_deleted(db, non_empty_group_list, random_group, app, check_ui):
