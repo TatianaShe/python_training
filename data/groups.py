50e369fb6c234ab652a7a0ef432f779e386ed78a
Tatiana Shestopalova <missis.dreamka@yandex.ru>
2022-01-08 21:29:54 +0300

    Изменен (v.5) файл requirements, добавлены настройки и разметка Allure.

--- data/groups.py
+++ data/groups.py
@@ -3,5 +3,6 @@ from model.group import Group
 
 test_data = [
     Group(name="name-1", header="header-1", footer="footer-1"),
-    Group(name="name-2", header="header-2", footer="footer-2")
+    Group(name="name-2", header="header-2", footer="footer-2"),
+    Group(name="name-3", header="header-3", footer="footer-3")
 ]
